f4403060f0c1812905da05bc70371efd254c4379
Ersel Aker <erselaker@gmail.com>
2018-04-16 02:26:02 +0100

    api

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const ArtistController = require('./controllers/Artist');
+
+require('dotenv').config({
+  path: path.join(__dirname, './settings.env'),
+});
+
+const app = express();
+mongoose.connect(process.env.DATABASE_CONN);
+
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Hello World!'));
+app.post('/Artist', ArtistController.post);
+app.get('/Artist', ArtistController.list);
+app.get('/Artist/:artistId', ArtistController.get);
+
+app.listen(3000, () => console.log('Music Library API is running'));
